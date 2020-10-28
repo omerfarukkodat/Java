d7ff92a23449600d686d5863cfb9ce591db1f354
Faruk Kodat <faru2kk@windowslive.com>
2020-10-28 20:24:12 +0300

    ...

---
+++ variables/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+//reusability(bir kere yaz her yerde kullan)
+    public static void main(String[] args) {
+    int sayi = 15;
+    //String mesaj = "öğrenci sayisi : ";
+	System.out.println("Öğrenci Sayım : " + sayi);
+    }
+}
